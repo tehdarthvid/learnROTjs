a319e5a6b76962885fe31c6aabecb1e2925ccf00
tehdarthvid <darthvid@hotmail.com>
2020-03-21 11:04:19 +0800

    updated fixed bundle and serve sequence

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@
 import copy from "rollup-plugin-copy";
 import livereload from "rollup-plugin-livereload";
 
+const production = !process.env.ROLLUP_WATCH;
+
 export default {
   input: "src/main.js",
   output: {
@@ -13,6 +15,31 @@ export default {
     copy({
       targets: [{ src: "build/index.html", dest: "dist" }]
     }),
-    livereload("dist")
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("dist")
+
+    //livereload("dist")
   ]
 };
+
+function serve() {
+  let started = false;
+
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
+
+        require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true
+        });
+      }
+    }
+  };
+}
